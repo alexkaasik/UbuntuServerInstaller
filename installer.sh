2b73f004cb232590a7063fc7694c145826d8ba9b
alexkaasik <aleks.kaasik@outlook.com>
2022-09-01 14:24:18 +0300

    update to V1.1

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@
 
 read service
 read -ra test <<< "$service"
-
+Arr=('0')
 #Remove_All=( ${test[@]/all/})
 #echo ${Remove_All[@]}
 
@@ -12,31 +12,15 @@ for i in "${test[@]}"; do
     if [[ "${test[i]}" == "all" ]]; then
         Arr=('0' '1' '2' '3' '4' '5' '6' '7' '8' '9')
         Remove_All=( ${test[@]/all/} )
-        echo ${Remove_All[@]}
         for i in "${Remove_All[@]}"; do
-            echo $i
             Arr=( ${Arr[@]/$i/} )
         done
+        break
+    else
+        Arr=( ${test[@]} )
     fi
 done
 
-echo ${Arr[@]}
-
-#echo ${Remove_Arr[@]}
-#
-#if [ $service == 'all' ]; then
-#    Arr=('0' '1' '2' '3' '4' '5' '-6' '7' '8' '9')
-#else
-#    read -ra Arr <<< "$service"
-#fi
-#
-#
-#for i in "${Arr[@]}"; do
-#   if [ $i < 0 ]; then
-#        printf "$i\n"
-#    fi
-#done
-
-for i in "${Arr[@]}"; do
-        printf "$i\n"
+for r in "${Arr[@]}"; do
+        printf "$r\n"
 done
