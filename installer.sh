c8cfca4de970faf72288cd82f550a297169d7e24
alexkaasik <aleks.kaasik@outlook.com>
2022-09-02 20:27:01 +0300

    update to V3.0(DNS added)

--- installer.sh
+++ installer.sh
@@ -10,14 +10,40 @@ read service
 read -ra service <<< "$service"
 
 function dhcp_call(){
-    sudo -S <<< $password apt install isc-dhcp-server
-    sudo -S <<< $password sed -i 's/INTERFACESv4=""/INTERFACESv4="enp0s8/' /etc/default/isc-dhcp-server
+    sudo -S <<< $password apt install -y isc-dhcp-server
+    sudo -S <<< $password sed -i 's/INTERFACESv4=""/INTERFACESv4="enp0s8"/' /etc/default/isc-dhcp-server
     sudo -S <<< $password mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.backup
     sudo -S <<< $password cp dhcp.txt /etc/dhcp/dhcpd.conf
 
     sudo -S <<< $password systemctl restart isc-dhcp-server
     sudo -S <<< $password systemctl status isc-dhcp-server
 }
+function samba_call(){
+    sudo -S <<< $password apt install -y samba
+    sudo -S <<< $password mv /etc/samba/smb.conf /etc/samba/smb.conf
+    sudo -S <<< $password cp samba.txt /etc/samba/smb.conf
+    sudo -S <<< $password Systemctl restart smbd
+    sudo -S <<< $password Systemctl enable smbd
+    sudo -S <<< $password Systemctl status smbd
+}
+function dns_call(){
+    sudo -S <<< $password apt install -y bind9 dnsutils
+    sudo -S <<< $password enable bind9
+    sudo -S <<< $password start bind9
+
+    sudo -S <<< $password dns_option.txt > /etc/bind/named.conf.option
+    
+    sudo -S <<< $password dns.localfor.txt >> /etc/bind/named.conf.local
+#   sudo -S <<< $password dns.localrev.txt >> /etc/bind/named.conf.local
+
+    sudo -S <<< $password mkdir -p /etc/bind/dns-zones 
+    sudo -S <<< $password cp forward.txt /etc/bind/dns-zones/kaasik.loc
+    sudo -S <<< $password cp reverse.txt /etc/bind/dns-zones/12.168.192-rev
+
+    sudo -S <<< $password systemctl restart bind9
+    sudo -S <<< $password systemctl status bind9 
+}
+
 
 for i in "${service[@]}"; do
     if [[ "${service[i]}" == "all" ]]; then
@@ -35,7 +61,11 @@ done
 for i in "${Arr[@]}"; do
     case $i in
         0)
-            (dhcp_call1);;
+            (dhcp_call);;
+        1)
+            (samba_call);;
+        2)
+            (dns_call);;
         *)
             echo "$i no service is available";;
     esac
